bce784d34a52ca86e65415beabb396a0a8d61eb3
Juscélio Reis <juscelioreis@wizsolucoes.com.br>
2019-11-20 12:36:05 -0300

    Preparando o projeto para gerar a release

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bot Template for Community
+This repository contains a bot template to facilitate community adoption. You can find additional [SDK V4](https://github.com/Microsoft/BotBuilder-Samples/tree/master/samples) in the BotBuilder-Samples repo.
+
+## Contributing
+
--- src/Bot.Template.Community.Api/Startup.cs
+++ src/Bot.Template.Community.Api/Startup.cs
@@ -48,7 +48,7 @@ namespace Bot.Template.Community.Api
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             app.UseHttpsRedirection();
