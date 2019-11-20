34f4192538dc7d00127df3c7d85eabcdbc7f886b
Juscélio Reis <juscelior@gmail.com>
2019-11-20 19:31:41 -0300

    ps1 for create vstemplate

---
+++ build/gen-vstemplate.ps1
@@ -0,0 +1,125 @@
+﻿# Microsoft PowerShell script to create a simple function
+# Author: Juscélio Reis
+function Append-node {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $fullName,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $name,
+         [Parameter(Mandatory=$true, Position=2)]
+         [xml] $vstemplate,
+         [Parameter(Mandatory=$true, Position=3)]
+         [string] $xdNS,
+         [Parameter(Mandatory=$false, Position=4)]
+         [int] $depth
+    )
+    
+    [System.Xml.XmlNode]$item = $null
+    if((Get-Item $fullName) -is [System.IO.DirectoryInfo]){
+        $item = $vstemplate.CreateNode("element","Folder",$xdNS)
+        $item.SetAttribute("Name",$name)
+        $item.SetAttribute("TargetFolderName",$name)
+        #$vstemplate.VSTemplate.TemplateContent.Project.AppendChild($folder)
+    }elseif(!$f.Name.EndsWith('.csproj')){
+        $item = $vstemplate.CreateNode("element","ProjectItem",$xdNS)
+        $item.SetAttribute("ReplaceParameters","true")
+        $item.SetAttribute("TargetFileName",$name)
+        $item.AppendChild($vstemplate.CreateTextNode($name))
+        #$vstemplate.VSTemplate.TemplateContent.Project.AppendChild($projectItem)
+    }
+
+    [System.Collections.ArrayList]$names = @()
+    
+    $files = Get-ChildItem $fullName -Recurse | Select-Object Name, FullName
+
+    foreach ($f in $files) {
+        if((Get-Item $f.FullName) -is [System.IO.DirectoryInfo]){
+            $sub = Append-node -fullName $fullName -name $name -vstemplate $vstemplate -xdNS $xdNS -depth ($depth + 1)
+            $item.AppendChild($sub)
+        }else{
+            $subItem = $vstemplate.CreateNode("element","ProjectItem",$xdNS)
+            $subItem.SetAttribute("ReplaceParameters","true")
+            $subItem.SetAttribute("TargetFileName",$f.Name)
+            $subItem.AppendChild($vstemplate.CreateTextNode($f.Name))
+            $item.AppendChild($subItem)
+        }
+    }
+
+    if($depth -eq 0){
+        $vstemplate.VSTemplate.TemplateContent.Project.AppendChild($item)
+    }
+
+    return $item
+}
+
+$PSScriptRoot = 'Z:\dev\bot-template\Bot.Template.Community\build'
+# Update NuGet package version
+$projectName = 'Bot.Template.Community'
+$FullPath = Resolve-Path $PSScriptRoot\..\src
+$buildDir = Resolve-Path $FullPath\..\build
+$templateDir = Resolve-Path $FullPath\..\build\template
+$ignore = @('bin', 'Bot.Template.Community', 'obj', '*.sln', '.*', '*.cache')
+
+$projects = Get-ChildItem $FullPath -Exclude $ignore | Select-Object Name, FullName
+
+Copy-Item -Path ($buildDir.Path + '/__TemplateIcon.ico') -Destination ($templateDir.Path + '/__TemplateIcon.ico') -Recurse -Force
+Copy-Item -Path ($buildDir.Path + '/Bot.Template.Community.vstemplate') -Destination ($templateDir.Path + '/Bot.Template.Community.vstemplate') -Recurse -Force
+
+foreach ($p in $projects) {
+    $files = Get-ChildItem $p.FullName | 
+                Where-Object{$_.Name -notin $ignore} | 
+                Select-Object Name, FullName
+    
+    New-Item -Path $templateDir.Path -Name $p.Name -ItemType "directory"
+    
+    #
+    [xml]$content = Get-Content ($templateDir.Path + '/Bot.Template.Community.vstemplate')
+    $xdNS = $content.DocumentElement.NamespaceURI
+    $projectTemplateLink = $content.CreateNode("element","ProjectTemplateLink",$xdNS)
+    $projectTemplateLink.SetAttribute("ProjectName","`$projectname`$" + ($p.Name -replace $projectName,""))
+    $projectTemplateLink.AppendChild($content.CreateTextNode($p.Name+"\MyTemplate.vstemplate"))
+    
+    $content.VSTemplate.TemplateContent.ProjectCollection.SolutionFolder.AppendChild($projectTemplateLink)
+    $content.Save(($templateDir.Path + '/Bot.Template.Community.vstemplate'))
+
+    #
+    Copy-Item -Path ($buildDir.Path + '/__TemplateIcon.ico') -Destination ($templateDir.Path + '/' + $p.Name) -Recurse -Force
+    Copy-Item -Path ($buildDir.Path + '/MyTemplate.vstemplate') -Destination ($templateDir.Path + '/' + $p.Name) -Recurse -Force
+
+    [xml]$vstemplate = Get-Content ($templateDir.Path + '/' + $p.Name + '/MyTemplate.vstemplate')
+    $xdNS = $vstemplate.DocumentElement.NamespaceURI
+    $vstemplate.VSTemplate.TemplateData.Description = $p.Name
+    $vstemplate.VSTemplate.TemplateContent.Project.SetAttribute("TargetFileName",$p.Name+'.csproj')
+    $vstemplate.VSTemplate.TemplateContent.Project.SetAttribute("File",$p.Name+'.csproj')
+
+    foreach($f in $files){
+        
+        Copy-Item -Path $f.FullName -Destination ($templateDir.Path + '/' + $p.Name) -Recurse -Force -Exclude $ignore  
+
+        if((Get-Item $f.FullName) -is [System.IO.DirectoryInfo]){          
+            Append-node -fullName $f.FullName -name $f.Name -vstemplate $vstemplate -xdNS $xdNS
+            #$vstemplate.VSTemplate.TemplateContent.Project.AppendChild($node)
+        }elseif(!$f.Name.EndsWith('.csproj')){
+            $projectItem = $vstemplate.CreateNode("element","ProjectItem",$xdNS)
+            $projectItem.SetAttribute("ReplaceParameters","true")
+            $projectItem.SetAttribute("TargetFileName",$f.Name)
+            $projectItem.AppendChild($vstemplate.CreateTextNode($f.Name))
+
+            $vstemplate.VSTemplate.TemplateContent.Project.AppendChild($projectItem)
+        }
+    }
+    
+    $vstemplate.Save(($templateDir.Path + '/' + $p.Name + '/MyTemplate.vstemplate'))
+}
+
+
+Add-Type -assembly "system.io.compression.filesystem"
+
+[io.compression.zipfile]::CreateFromDirectory($templateDir, ($FullPath.Path + '\Bot.Template.Community\' + $projectName + '.zip'))
+
+
+
+
+        
+    
