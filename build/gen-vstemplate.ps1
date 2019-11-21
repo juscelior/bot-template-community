29a0201340e92ea2d3d66c851e555320290ff833
Juscélio Reis <juscelior@gmail.com>
2019-11-20 23:59:24 -0300

    correcting script

--- build/gen-vstemplate.ps1
+++ build/gen-vstemplate.ps1
@@ -53,7 +53,7 @@ function Append-node {
 }
 
 #onlylocal
-#$PSScriptRoot = 'Z:\dev\bot-template\Bot.Template.Community\build'
+$PSScriptRoot = 'Z:\dev\bot-template\Bot.Template.Community\build'
 # Update NuGet package version
 $projectName = 'Bot.Template.Community'
 $FullPath = Resolve-Path $PSScriptRoot\..\src
@@ -70,22 +70,32 @@ $projects = Get-ChildItem $FullPath -Exclude $ignore | Select-Object Name, FullN
 Copy-Item -Path ($buildDir.Path + '/__TemplateIcon.ico') -Destination ($templateDir.Path + '/__TemplateIcon.ico') -Recurse -Force
 Copy-Item -Path ($buildDir.Path + '/Bot.Template.Community.vstemplate') -Destination ($templateDir.Path + '/Bot.Template.Community.vstemplate') -Recurse -Force
 
+#
+[xml]$content = Get-Content ($templateDir.Path + '/Bot.Template.Community.vstemplate')
+$xdNS = $content.DocumentElement.NamespaceURI
+
+$customParameter = $content.CreateNode("element","CustomParameter",$xdNS)
+$customParameter.SetAttribute("Name","`$safeprojectname`$")
+$customParameter.SetAttribute("Value",$projectName)
+
+$customParameters = $content.CreateNode("element","CustomParameters",$xdNS)
+$customParameters.AppendChild($customParameter)
+
+$content.VSTemplate.TemplateContent.AppendChild($customParameters)
+
 foreach ($p in $projects) {
     $files = Get-ChildItem $p.FullName | 
                 Where-Object{$_.Name -notin $ignore} | 
                 Select-Object Name, FullName
     
     New-Item -Path $templateDir.Path -Name $p.Name -ItemType "directory"
-    
-    #
-    [xml]$content = Get-Content ($templateDir.Path + '/Bot.Template.Community.vstemplate')
-    $xdNS = $content.DocumentElement.NamespaceURI
+
     $projectTemplateLink = $content.CreateNode("element","ProjectTemplateLink",$xdNS)
     $projectTemplateLink.SetAttribute("ProjectName","`$projectname`$" + ($p.Name -replace $projectName,""))
+    $projectTemplateLink.SetAttribute("CopyParameters","True")
     $projectTemplateLink.AppendChild($content.CreateTextNode($p.Name+"\MyTemplate.vstemplate"))
     
     $content.VSTemplate.TemplateContent.ProjectCollection.SolutionFolder.AppendChild($projectTemplateLink)
-    $content.Save(($templateDir.Path + '/Bot.Template.Community.vstemplate'))
 
     #
     Copy-Item -Path ($buildDir.Path + '/__TemplateIcon.ico') -Destination ($templateDir.Path + '/' + $p.Name) -Recurse -Force
@@ -112,12 +122,14 @@ foreach ($p in $projects) {
 
             $vstemplate.VSTemplate.TemplateContent.Project.AppendChild($projectItem)
         }elseif($f.Name.EndsWith('.csproj')){
-            ((Get-Content -path ($templateDir.Path + '/' + $p.Name + '/' + $f.Name) -Raw) -replace $projectName,"`$projectname`$") | Set-Content -Path ($templateDir.Path + '/' + $p.Name + '/' + $f.Name)
+            ((Get-Content -path ($templateDir.Path + '/' + $p.Name + '/' + $f.Name) -Raw) -replace $projectName,"`$ext_safeprojectname`$") | Set-Content -Path ($templateDir.Path + '/' + $p.Name + '/' + $f.Name)
         }
     }
     
     $vstemplate.Save(($templateDir.Path + '/' + $p.Name + '/MyTemplate.vstemplate'))
 }
+
+$content.Save(($templateDir.Path + '/Bot.Template.Community.vstemplate'))
 
 
 Add-Type -assembly "system.io.compression.filesystem"
